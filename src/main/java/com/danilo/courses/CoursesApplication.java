9541b34b94b76c19c72a11120c65455916ae1753
Danilo Souza <danilosouza.bm@gmail.com>
2021-12-05 13:45:24 -0300

    Testing CoursesApplication

--- src/main/java/com/danilo/courses/CoursesApplication.java
+++ src/main/java/com/danilo/courses/CoursesApplication.java
@@ -1,13 +1,29 @@
 package com.danilo.courses;
 
+import com.danilo.courses.entities.Course;
+import com.danilo.courses.repositories.CourseRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CoursesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursesApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDB(CourseRepository courseRepository){
+		return args -> {
+			Course course = new Course();
+			course.setName("Curso Angular");
+			course.setCategory("Front-end");
+			courseRepository.deleteAll();
+			courseRepository.save(course);
+		};
 	}
 
 }
